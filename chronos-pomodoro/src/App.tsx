cd998e78d359ec26525f947698ae52db893425ec
SasKarol <karoline.s.silva@iesb.edu.br>
2026-03-24 20:33:36 -0300

    Projeto Promodoro - Prática 15

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,10 +1,10 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
-
 import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -20,12 +20,13 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
-      
+
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
             <label htmlFor='meuInput'>task</label>
             <input id='meuInput' type='text' />
+            <DefaultInput id='meuInput' type='text' />
           </div>
 
           <div className='formRow'>
